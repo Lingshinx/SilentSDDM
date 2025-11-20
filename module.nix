7729196e4def8f543754f9ad65ed1924e684395e
lingshin <yokaringx@outlook.com>
2025-11-21 00:48:18 +0800

    use flake.part

--- module.nix
+++ module.nix
@@ -1,17 +1,19 @@
-{pkgs, config, lib, inputs, ...}:
-
+{self}: {
+  pkgs,
+  config,
+  lib,
+  ...
+}:
 with builtins;
 with lib;
-with types;
-
-{
+with types; {
   options.services.displayManager.sddm.silent = {
     theme = mkOption {
       type = nullOr path;
       default = null;
       description = ''
         SilentSDDM config file
-        '';
+      '';
     };
 
     extraBackgrounds = mkOption {
@@ -27,17 +29,18 @@ with types;
     silent = config.services.displayManager.sddm.silent;
     theme = config.services.displayManager.sddm.theme;
     enabled = theme == silent-sddm.pname;
-    silent-sddm = inputs.silentSDDM.packages.${pkgs.stdenv.hostPlatform.system}.default.override {
+    silent-sddm = self.packages.${pkgs.stdenv.hostPlatform.system}.default.override {
       inherit (silent) theme extraBackgrounds;
     };
-  in mkIf enabled {
-    environment.systemPackages = silent-sddm;
-    services.displayManager.sddm = {
-      extraPackages = silent-sddm.propagatedBuildInputs;
-      settings.General = {
-        GreeterEnvironment = "QML2_IMPORT_PATH=${silent-sddm}/share/sddm/themes/${silent-sddm.pname}/components/,QT_IM_MODULE=qtvirtualkeyboard";
-        InputMethod = "qtvirtualkeyboard";
+  in
+    mkIf enabled {
+      environment.systemPackages = [silent-sddm];
+      services.displayManager.sddm = {
+        extraPackages = silent-sddm.propagatedBuildInputs;
+        settings.General = {
+          GreeterEnvironment = "QML2_IMPORT_PATH=${silent-sddm}/share/sddm/themes/${silent-sddm.pname}/components/,QT_IM_MODULE=qtvirtualkeyboard";
+          InputMethod = "qtvirtualkeyboard";
+        };
       };
     };
-  };
 }
